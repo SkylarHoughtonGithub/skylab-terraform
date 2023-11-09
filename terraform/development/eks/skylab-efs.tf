43a0b4b1df8067b4f0b5e5ca7ccedfc5166c2715
Skylar Houghton <skylarhoughtongithub@gmail.com>
2023-11-08 21:01:35 -0600

    Update skylab-efs.tf

--- terraform/development/eks/skylab-efs.tf
+++ terraform/development/eks/skylab-efs.tf
@@ -4,7 +4,7 @@ module "efs" {
   # File system
   name      = "skylab-efs"
   encrypted = false
-  #   kms_key_arn = "arn:aws:kms:us-east-2:959946720450:key/0e8632b9-bbd4-460b-ad03-fad2bff8a5d7"
+  #   kms_key_arn = "arn:aws:kms:us-east-2:11111111111:key/0e8632b9-bbd4-460b-ad03-fad2bff8a5d7"
 
   performance_mode = "generalPurpose"
   lifecycle_policy = {
@@ -21,7 +21,7 @@ module "efs" {
       principals = [
         {
           type        = "AWS"
-          identifiers = ["arn:aws:iam::959946720450:role/cross_account_admin"]
+          identifiers = ["arn:aws:iam::11111111111:role/admin"]
         }
       ]
     }
@@ -30,14 +30,14 @@ module "efs" {
   # Mount targets / security group
   mount_targets = {
     "platform-dev-pri-ext-use2c-1" = {
-      subnet_id = "subnet-05af4226d2e26eee7"
+      subnet_id = "subnet-d1d1d1d1d1d1d1d1d"
     }
     "platform-dev-pri-ext-use2b-1" = {
-      subnet_id = "subnet-0819ff61677c225a6"
+      subnet_id = "subnet-d1d1d1d1d1d1d1d12"
     }
   }
   security_group_description = "skylab EFS security group"
-  security_group_vpc_id      = "vpc-04d7e22454efae807"
+  security_group_vpc_id      = "vpc-2d2d2d2d2d2d2d"
   security_group_rules = {
     vpc = {
       # relying on the defaults provided for EFS/NFS (2049/TCP + ingress)
