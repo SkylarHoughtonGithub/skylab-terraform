b1433b70869ab406f0e44f6552067da9fcb3f9a2
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2024-01-19 16:59:41 -0600

    added docker, sonarqube and trivy to user_data

--- terraform/development/aws/ec2/variables.tf
+++ terraform/development/aws/ec2/variables.tf
@@ -12,14 +12,27 @@ locals {
       user_data                   = <<-EOT
         #!/bin/bash
         sudo yum update -y
-        sudo yum install wget
+        sudo yum install wget -y
         sudo amazon-linux-extras install java-openjdk11
         sudo amazon-linux-extras install epel -y
+        sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
         sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+        sudo curl https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl -o "kubectl"
+        sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-        sudo yum install jenkins -y
+        sudo yum install docker jenkins terraform unzip -y
+        usermod -aG docker ec2-user
         sudo systemctl enable jenkins
+        sudo systemctl enable docker
         sudo systemctl start jenkins
+        sudo systemctl start docker
+        unzip awscliv2.zip
+        sudo ./aws/install
+        sudo chmod +x ./kubectl
+        sudo chmod 777 ./kubectl
+        sudo mv kubectl /usr/local/bin/
+        sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
+        sudo docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
         EOT
       root_block_device = [
         {
---
+++ terraform/development/aws/s3/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket         = "skylab-platform-artifacts"
+    region         = "us-east-2"
+    key            = "tfstate/dev/skylab/s3/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "LockID"
+  }
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
