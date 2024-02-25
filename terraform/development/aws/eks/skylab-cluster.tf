a0237c0e520e8eeaed4020d1728d73b5640ba9a4
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2024-02-25 13:01:40 -0600

    cleaned up eks deployment

--- terraform/development/aws/eks/skylab-cluster.tf
+++ terraform/development/aws/eks/skylab-cluster.tf
@@ -2,26 +2,23 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  #TODO: Build kubernetes provider to be capable of loop
-  # for_each = local.us_east_1
+  cluster_version                         = local.us_east_1.cluster_version
+  cluster_endpoint_public_access          = local.us_east_1.cluster_endpoint_public_access
+  cluster_name                            = local.us_east_1.cluster_name
+  cluster_addons                          = try(local.us_east_1.cluster_addons, null)
+  create_aws_auth_configmap               = try(local.us_east_1.create_aws_auth_configmap, false)
+  manage_aws_auth_configmap               = try(local.us_east_1.manage_aws_auth_configmap, false)
+  create_iam_role                         = try(local.us_east_1.create_iam_role, false)
+  iam_role_name                           = local.us_east_1.iam_role_name
+  aws_auth_roles                          = try(local.us_east_1.aws_auth_roles, null)
+  eks_managed_node_groups                 = local.us_east_1.eks_managed_node_groups
+  eks_managed_node_group_defaults         = local.us_east_1.eks_managed_node_group_defaults
+  cluster_security_group_additional_rules = try(local.us_east_1.cluster_security_group_additional_rules, null)
+  node_security_group_additional_rules    = try(local.us_east_1.node_security_group_additional_rules, null)
+  subnet_ids                              = local.us_east_1.subnet_ids
+  vpc_id                                  = local.us_east_1.vpc_id
 
-  cluster_version                         = local.cluster_version
-  cluster_endpoint_public_access          = local.cluster_endpoint_public_access
-  cluster_name                            = local.cluster_name
-  cluster_addons                          = try(local.cluster_addons, null)
-  create_aws_auth_configmap               = try(local.create_aws_auth_configmap, false)
-  manage_aws_auth_configmap               = try(local.manage_aws_auth_configmap, false)
-  create_iam_role                         = try(local.create_iam_role, false)
-  iam_role_name                           = local.iam_role_name
-  aws_auth_roles                          = try(local.aws_auth_roles, null)
-  eks_managed_node_groups                 = local.eks_managed_node_groups
-  eks_managed_node_group_defaults         = local.eks_managed_node_group_defaults
-  cluster_security_group_additional_rules = try(local.cluster_security_group_additional_rules, null)
-  node_security_group_additional_rules    = try(local.node_security_group_additional_rules, null)
-  subnet_ids                              = local.subnet_ids
-  vpc_id                                  = local.vpc_id
-
-  tags = local.tags
+  tags = local.us_east_1.tags
 
 
   providers = {
--- terraform/development/aws/eks/variables.tf
+++ terraform/development/aws/eks/variables.tf
@@ -3,143 +3,145 @@ variable "eks_api_allowed_cidrs" {
 }
 
 locals {
-  cluster_version                = "1.27"
-  cluster_endpoint_public_access = true
-  cluster_name                   = "skylab"
-  cluster_addons = {
-    coredns = {
-      most_recent                 = true
-      preserve                    = true
-      resolve_conflicts_on_create = true
+  us_east_1 = {
+    cluster_version                = "1.27"
+    cluster_endpoint_public_access = true
+    cluster_name                   = "skylab"
+    cluster_addons = {
+      coredns = {
+        most_recent                 = true
+        preserve                    = true
+        resolve_conflicts_on_create = true
+      }
+      kube-proxy = {
+        most_recent                 = true
+        preserve                    = true
+        resolve_conflicts_on_create = true
+      }
+      vpc-cni = {
+        most_recent                 = true
+        preserve                    = true
+        resolve_conflicts_on_create = true
+      }
     }
-    kube-proxy = {
-      most_recent                 = true
-      preserve                    = true
-      resolve_conflicts_on_create = true
+    create_aws_auth_configmap = true #terraform import module.eks.kubernetes_config_map.aws_auth[0] kube-system/aws-auth
+    manage_aws_auth_configmap = true #https://stackoverflow.com/questions/69873472/configmaps-aws-auth-already-exists
+    create_iam_role           = true
+    iam_role_name             = "skylab-managed-node-group-role"
+    aws_auth_roles = [
+      {
+        rolearn  = "arn:aws:iam::635314249418:user/chicken"
+        username = "chicken"
+        groups   = ["system:masters"]
+      },
+      {
+        rolearn  = "arn:aws:iam::635314249418:user/tetris"
+        username = "tetris"
+        groups   = ["system:masters"]
+      },
+    ]
+    eks_managed_node_groups = {
+      medium = {
+        min_size       = 1
+        max_size       = 3
+        desired_size   = 3
+        disk_size      = 3
+        disk_type      = "gp3"
+        instance_types = ["t3.medium"]
+        capacity_type  = "ON_DEMAND"
+      }
     }
-    vpc-cni = {
-      most_recent                 = true
-      preserve                    = true
-      resolve_conflicts_on_create = true
+    eks_managed_node_group_defaults = {
+      ami_type = "AL2_x86_64"
     }
-  }
-  create_aws_auth_configmap = true #terraform import module.eks.kubernetes_config_map.aws_auth[0] kube-system/aws-auth
-  manage_aws_auth_configmap = true #https://stackoverflow.com/questions/69873472/configmaps-aws-auth-already-exists
-  create_iam_role           = true
-  iam_role_name             = "skylab-managed-node-group-role"
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::635314249418:user/chicken"
-      username = "chicken"
-      groups   = ["system:masters"]
-    },
-    {
-      rolearn  = "arn:aws:iam::635314249418:user/tetris"
-      username = "tetris"
-      groups   = ["system:masters"]
-    },
-  ]
-  eks_managed_node_groups = {
-    medium = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 3
-      disk_size      = 3
-      disk_type      = "gp3"
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-    }
-  }
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
-  cluster_security_group_additional_rules = {
-    ingress_source_security_group_id = {
-      description = "EKS Cluster Private API Access"
-      protocol    = "tcp"
-      from_port   = 443
-      to_port     = 443
-      type        = "ingress"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  node_security_group_additional_rules = {
-    ingress_self_all = {
-      description = "Node to node all ports/protocols"
-      protocol    = "-1"
-      from_port   = 0
-      to_port     = 0
-      type        = "ingress"
-      self        = true
-    }
-    dns-tcp = {
-      description = "dns-tcp"
-      protocol    = "tcp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      cidr_blocks = var.eks_api_allowed_cidrs
-    }
-    dns-udp = {
-      description = "dns-udp"
-      protocol    = "udp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      cidr_blocks = var.eks_api_allowed_cidrs
+    cluster_security_group_additional_rules = {
+      ingress_source_security_group_id = {
+        description = "EKS Cluster Private API Access"
+        protocol    = "tcp"
+        from_port   = 443
+        to_port     = 443
+        type        = "ingress"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
     }
 
-    http = {
-      description = "http"
-      protocol    = "tcp"
-      from_port   = 80
-      to_port     = 80
-      type        = "ingress"
-      cidr_blocks = var.eks_api_allowed_cidrs
+    node_security_group_additional_rules = {
+      ingress_self_all = {
+        description = "Node to node all ports/protocols"
+        protocol    = "-1"
+        from_port   = 0
+        to_port     = 0
+        type        = "ingress"
+        self        = true
+      }
+      dns-tcp = {
+        description = "dns-tcp"
+        protocol    = "tcp"
+        from_port   = 53
+        to_port     = 53
+        type        = "ingress"
+        cidr_blocks = var.eks_api_allowed_cidrs
+      }
+      dns-udp = {
+        description = "dns-udp"
+        protocol    = "udp"
+        from_port   = 53
+        to_port     = 53
+        type        = "ingress"
+        cidr_blocks = var.eks_api_allowed_cidrs
+      }
+
+      http = {
+        description = "http"
+        protocol    = "tcp"
+        from_port   = 80
+        to_port     = 80
+        type        = "ingress"
+        cidr_blocks = var.eks_api_allowed_cidrs
+      }
+
+      https = {
+        description = "https"
+        protocol    = "tcp"
+        from_port   = 443
+        to_port     = 443
+        type        = "ingress"
+        cidr_blocks = var.eks_api_allowed_cidrs
+      }
+
+      # grafana = {
+      #   description = "grafana"
+      #   protocol    = "tcp"
+      #   from_port   = 3000
+      #   to_port     = 3000
+      #   type        = "ingress"
+      #   cidr_blocks = var.eks_api_allowed_cidrs
+      # }
+      # prometheus = {
+      #   description = "prometheus"
+      #   protocol    = "tcp"
+      #   from_port   = 9000
+      #   to_port     = 9100
+      #   type        = "ingress"
+      #   cidr_blocks = var.eks_api_allowed_cidrs
+      # }
+      # efs = {
+      #   description = "efs"
+      #   protocol    = "tcp"
+      #   from_port   = 2049
+      #   to_port     = 2049
+      #   type        = "ingress"
+      #   cidr_blocks = var.eks_api_allowed_cidrs
+      # }
+
     }
-
-    https = {
-      description = "https"
-      protocol    = "tcp"
-      from_port   = 443
-      to_port     = 443
-      type        = "ingress"
-      cidr_blocks = var.eks_api_allowed_cidrs
+    subnet_ids = ["subnet-04610b8b2e54b8972", "subnet-0f91e00155d822ebd"]
+    vpc_id     = "vpc-0f6c219833fe92be7"
+    tags = {
+      Name        = "skylab"
+      Environment = "dev"
+      Project     = "skylab"
+      CodeManaged = true
     }
-
-    # grafana = {
-    #   description = "grafana"
-    #   protocol    = "tcp"
-    #   from_port   = 3000
-    #   to_port     = 3000
-    #   type        = "ingress"
-    #   cidr_blocks = var.eks_api_allowed_cidrs
-    # }
-    # prometheus = {
-    #   description = "prometheus"
-    #   protocol    = "tcp"
-    #   from_port   = 9000
-    #   to_port     = 9100
-    #   type        = "ingress"
-    #   cidr_blocks = var.eks_api_allowed_cidrs
-    # }
-    # efs = {
-    #   description = "efs"
-    #   protocol    = "tcp"
-    #   from_port   = 2049
-    #   to_port     = 2049
-    #   type        = "ingress"
-    #   cidr_blocks = var.eks_api_allowed_cidrs
-    # }
-
-  }
-  subnet_ids = ["subnet-04610b8b2e54b8972", "subnet-0f91e00155d822ebd"]
-  vpc_id     = "vpc-0f6c219833fe92be7"
-  tags = {
-    Name        = "skylab"
-    Environment = "dev"
-    Project     = "skylab"
-    CodeManaged = true
   }
 }
