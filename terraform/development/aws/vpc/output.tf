bc3a3a525a9618795429e2104c0735d62ea591ab
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2025-06-14 18:33:33 -0500

    use1 to use2 prefix

--- terraform/development/aws/vpc/output.tf
+++ terraform/development/aws/vpc/output.tf
@@ -1,3 +1,3 @@
-output "skylab-use1-vpc-id" { value = module.vpc_use2["skylab"].vpc_id }
-output "skylab-use1-private-subnet-ids" { value = module.vpc_use2["skylab"].private_subnets }
-output "skylab-use1-public-subnet-ids" { value = module.vpc_use2["skylab"].public_subnets }
+output "skylab-use2-vpc-id" { value = module.vpc_use2["skylab"].vpc_id }
+output "skylab-use2-private-subnet-ids" { value = module.vpc_use2["skylab"].private_subnets }
+output "skylab-use2-public-subnet-ids" { value = module.vpc_use2["skylab"].public_subnets }
